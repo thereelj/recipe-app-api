0dbc08cf540eb6ec670119e3af148e527fe3defc
Jez <nxt.be.dev.two@gmail.com>
2022-04-01 09:48:45 -0400

    Adding python tests

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,18 @@
+from django.forms import PasswordInput
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a user with an email successful"""
+        email = 'test@gmail.com'
+        password = 'Testp@ss123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=PasswordInput
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
